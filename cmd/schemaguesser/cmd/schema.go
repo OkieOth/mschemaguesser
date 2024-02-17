fbe19ba46710579e666d67d18a8d00efdb5e0fcb
Eiko Thomas <eiko.thomas@gmail.com>
2024-02-17 19:08:34 +0100

    implement 'all' option for 'schema' command

--- cmd/schemaguesser/cmd/schema.go
+++ cmd/schemaguesser/cmd/schema.go
@@ -22,20 +22,16 @@ var schemaCmd = &cobra.Command{
 	Short: "functions around the schemas",
 	Long:  "With this command you can create schemas out of mongodb collection",
 	Run: func(cmd *cobra.Command, args []string) {
-		bsonRaw, err := mongoHelper.QueryCollection(mongoHelper.ConStr, dbName, colName, int(itemCount))
-		if err != nil {
-			msg := fmt.Sprintf("Error while reading indexes for collection (%s.%s): \n%v\n", databaseName, colName, err)
-			panic(msg)
-		}
-		var otherComplexTypes []mongoHelper.ComplexType
-		var mainType mongoHelper.ComplexType
-		for _, b := range bsonRaw {
-			err = mongoHelper.ProcessBson(b, colName, &mainType, &otherComplexTypes)
-			if err != nil {
-				fmt.Printf("Error while processing bson for schema: %v", err)
+		if dbName == "all" {
+			printSchemasForAllDatabases()
+		} else {
+			if colName == "all" {
+				printSchemasForAllCollections(dbName)
+			} else {
+				printSchemaForOneCollection(dbName, colName)
 			}
 		}
-		schema.PrintSchema(dbName, colName, &mainType, otherComplexTypes, outputDir)
+
 	},
 }
 
@@ -47,4 +43,35 @@ func init() {
 	schemaCmd.Flags().StringVar(&outputDir, "output", "stdout", "stdout or the directory to write the created schema file, default is 'stdout'")
 
 	schemaCmd.Flags().Int32Var(&itemCount, "item_count", 100, "Number of collection entries used to build the schema")
+}
+
+func printSchemaForOneCollection(dbName string, collName string) {
+	bsonRaw, err := mongoHelper.QueryCollection(mongoHelper.ConStr, dbName, collName, int(itemCount))
+	if err != nil {
+		msg := fmt.Sprintf("Error while reading data for collection (%s.%s): \n%v\n", dbName, collName, err)
+		panic(msg)
+	}
+	var otherComplexTypes []mongoHelper.ComplexType
+	var mainType mongoHelper.ComplexType
+	for _, b := range bsonRaw {
+		err = mongoHelper.ProcessBson(b, collName, &mainType, &otherComplexTypes)
+		if err != nil {
+			fmt.Printf("Error while processing bson for schema: %v", err)
+		}
+	}
+	schema.PrintSchema(dbName, collName, &mainType, otherComplexTypes, outputDir)
+}
+
+func printSchemasForAllCollections(dbName string) {
+	collections := mongoHelper.ReadCollectionsOrPanic(dbName)
+	for _, coll := range *collections {
+		printSchemaForOneCollection(dbName, coll)
+	}
+}
+
+func printSchemasForAllDatabases() {
+	dbs := mongoHelper.ReadDatabasesOrPanic()
+	for _, db := range *dbs {
+		printSchemasForAllCollections(db)
+	}
 }
